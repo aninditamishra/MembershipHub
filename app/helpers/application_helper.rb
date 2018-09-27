633be6bba94bcb8a28a2764760f5620f92ce5247
domfabian <dominickfabian@gmail.com>
2018-09-27 17:27:54 +0000

    simplify some page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+    # return the full page title on a per-page basis
+    def full_title(page_title = "")
+        title_base = "MembershipHub"
+        if page_title.empty?
+            title_base
+        else
+            page_title + " | #{title_base}"
+        end
+    end
 end
