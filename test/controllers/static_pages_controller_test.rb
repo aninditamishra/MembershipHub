4e9f53a0f706bd81835a21ff8c92aacee4af6c37
domfabian <dominickfabian@gmail.com>
2018-09-27 17:14:51 +0000

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @title_base = "| MembershipHub"
+  end
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home #{@title_base}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About #{@title_base}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help #{@title_base}"
   end
 
 end
